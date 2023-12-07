6813d7f0f155fdeebfd9bebb6abf8d8ba189b056
Барышев Степан Сергеевич <Baryshev.S.S@corp.kp11.ru>
2023-12-07 16:18:21 +0300

    0.2.4

--- Assets/Scripts/Game/Dialogue/DialogueManagement/DialogueChoice.cs
+++ Assets/Scripts/Game/Dialogue/DialogueManagement/DialogueChoice.cs
@@ -43,11 +43,11 @@ public class DialogueChoice : MonoBehaviour
 
     public void HideChoises()
     {
-        foreach(var button in _choices) // 1 способ
-        {
-            button.SetActive(false);
-        }
+        //foreach(var button in _choices) // 1 способ
+        //{
+        //    button.SetActive(false);
+        //}
 
-        //Array.ForEach(_choices, button => { button.SetActive(false); }); // 2 способ
+        Array.ForEach(_choices, button => { button.SetActive(false); }); // 2 способ
     }
 }
