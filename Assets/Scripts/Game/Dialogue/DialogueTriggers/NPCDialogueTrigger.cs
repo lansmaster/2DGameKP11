d0ba7d4c691f4e66cf8cdf08cdbe52c6d04ee89c
lansmaster <stepanbaryhev@gmail.com>
2023-12-16 17:37:54 +0300

    0.4

--- Assets/Scripts/Game/Dialogue/DialogueTriggers/NPCDialogueTrigger.cs
+++ Assets/Scripts/Game/Dialogue/DialogueTriggers/NPCDialogueTrigger.cs
@@ -7,14 +7,24 @@ public class NPCDialogueTrigger : MonoBehaviour
     private DialogueController _dialogueController;
     private DialogueWindow _dialogueWindow;
 
+    private Animator _animator;
     private void Start()
     {
+        _animator = GetComponent<Animator>();
+
         _dialogueController = FindObjectOfType<DialogueController>();
         _dialogueWindow = FindObjectOfType<DialogueWindow>();
     }
 
+    private void FixedUpdate()
+    {
+        _animator.SetBool("Emission", false);
+    }
+
     public void TriggerAction()
     {
+        _animator.SetBool("Emission", true);
+
         if (Input.GetKeyDown(KeyCode.E) && !_dialogueWindow.IsPlaying)
         {
             _dialogueController.EnterDialogueMode(_inkJSON);
